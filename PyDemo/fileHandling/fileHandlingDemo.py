53896feca655209d9c5359def6b337b796e3bd27
Amol <amolbinwade@gmail.com>
2018-01-18 01:04:36 +0530

    adding changes

--- PyDemo/fileHandling/fileHandlingDemo.py
+++ PyDemo/fileHandling/fileHandlingDemo.py
@@ -1,9 +1,17 @@
 import os
+from pathlib import Path
 
 path = '/home/amol/temp/test'
 print(os.path.exists(path))
 
-file1 = open(path+'/test.txt', 'r')
-file2 = open('test1.txt', 'w')
-for line in file1:
-    file2.write(line.replace('jumps', 'bumps'))
+# file1 = open(path+'/test.txt', 'r')
+# file2 = open('test1.txt', 'w')
+# for line in file1:
+#     file2.write(line.replace('jumps', 'bumps'))
+
+loc = Path(path)
+for p in loc.iterdir():
+        print(p)
+        if p.is_file():
+            print("true")
+            print(open(str(p), 'r').read())
